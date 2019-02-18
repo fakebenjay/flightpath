9364a238e22afaf43ef7b0365ee0c573a70e9e35
fakebenjay <fakebenjay@gmail.com>
2019-02-18 02:30:47 -0500

    index.js update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import { store } from './store'
 import { Provider } from 'react-redux'
 import ConnectedApp from './App'
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory'
+
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL })
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedApp />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store = { store }>
+	<ConnectedApp />
+	</Provider>,
+	document.getElementById('root') at HTMLElement
 )
